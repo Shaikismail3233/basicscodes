3ca218062e26fa08c72141fba5b8b335923acc72
Shaikismail3233 <153603834+Shaikismail3233@users.noreply.github.com>
2023-12-18 02:18:12 +0700

    Add files via upload

---
+++ src/ndsn/Ascending.java
@@ -0,0 +1,26 @@
+package Arrayordering;
+
+import java.util.Arrays ;
+import java.util.Collections;
+
+
+
+public class Ascending {
+	
+	public static void main(String[] args) {
+	
+		
+int[] arr = {24,87,65,98,78,48,77};
+
+System.out.println("original array is " +  Arrays.toString(arr));
+Arrays.sort(arr);
+System.out.println("Sorted array is " + Arrays.toString((arr)));
+
+
+
+		
+		
+		
+		
+}
+}
